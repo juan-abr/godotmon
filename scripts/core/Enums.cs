c720a7f7d7e2fa3a18d7c19a934518ede5846f21
Juan R <juanabarro@protonmail.com>
2025-10-07 12:26:29 -0400

    made corrections to level names

--- scripts/core/Enums.cs
+++ scripts/core/Enums.cs
@@ -32,7 +32,8 @@ namespace Game.Core
         small_town,
         small_town_greens_house,
         small_town_purples_house,
-        small_town_pokemon_center,
+        // Not added.
+        // small_town_pokemon_center,
         small_town_cave,
     }
 
